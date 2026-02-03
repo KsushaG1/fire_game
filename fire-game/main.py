47817575f2389a15f3acc72cc7ed64def0a3e9e2
KsushaG1 <149872580+KsushaG1@users.noreply.github.com>
2026-02-03 17:05:50 +0300

    Add files via upload
    My first game. 
    In this game, you will control a helicopter that must extinguish fires that occur near trees. You will also earn points for each extinguished tree. It is possible to expand the water capacity of the helicopter, which is used to extinguish the fire, for the points received. There is an upgrade store on the map for this purpose.

---
+++ fire-game/main.py
@@ -0,0 +1,48 @@
+# 🟩🌳💧🚁🔥🏩❤️🏪☁️🌩️⚡🏆🪣
+from pynput import keyboard
+from map import Map
+import time
+import os
+from helicopter import Helicopter as Helico
+
+
+TICK_SLEEP = 0.05
+THREE_UPDATE = 50
+FIRE_UPDATE = 75
+MAP_W, MAP_H = 20, 10
+
+
+
+field = Map(MAP_W, MAP_H)
+
+
+MOVES = {'w': (-1, 0), 'd': (0, 1), 's': (1, 0), 'a': (0, -1)}
+def process_key(key):
+    global helico
+    c = key.char
+    if c in MOVES.keys():
+      dx, dy = MOVES[c][0], MOVES[c][1]
+      helico.move(dx, dy)
+listener = keyboard.Listener(
+    on_press=None,
+    on_release=process_key,)
+listener.start()
+
+
+
+helico = Helico(MAP_W, MAP_H)
+
+tick = 1
+
+while True:
+  os.system('cls')
+  print('TICK', tick)
+  field.process_helicopter(helico)
+  helico.print_stats()
+  field.print_map(helico)
+  tick += 1
+  time.sleep(TICK_SLEEP)
+  if(tick % THREE_UPDATE == 0):
+    field.generate_tree()
+  if (tick % FIRE_UPDATE == 0):
+    field.update_fires()
